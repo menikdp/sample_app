04d79ae33ace97a950ac02b6122bb8d866e26a90
menikdp <menik.damayanti.p@gmail.com>
2015-02-03 11:13:31 +0700

    finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -46,7 +46,7 @@ describe "Static pages" do
   describe "Contact page" do
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_css('h1',
+      page.should have_css('h1', 
         :text => 'Contact')
     end
 
